66f547e9f99a66b5c3f02e4cd5287dad94b6ebdd
tblackmore0 <tblackmore0@gmail.com>
2021-01-26 17:44:58 +0000

    pretty much all done
    all done, could add more functionality for sure, but not sure if it's worth the time

--- sketch.js
+++ sketch.js
@@ -3,25 +3,36 @@ function makeRows(rows, cols) {
     container.style.setProperty("--grid-cols", cols);
     for (c = 0; c < (rows * cols); c++) {
       let cell = document.createElement("div");
+      let randomColor = Math.floor(Math.random()*16777215).toString(16);
       container.appendChild(cell).className = "grid-item";
+      cell.onmouseover = function () {
+        this.style.backgroundColor = "#" + randomColor;
+      }
     };
   };
- 
-
 
 
 window.onload = function () {
     let container = document.getElementById("container");
-    makeRows(16, 16);
-    setHover ();
+    let button = document.getElementById ("resetButton");
+    button.addEventListener("click", reset);
+    makeRows(16, 16);    
   };
 
 
-function setHover (){
-  items = document.getElementsByClassName ("grid-item");
-  for (i=0; i<items.length; i++) {
-    items[i].onmouseover = function () {
-      this.className = "itemsBlack";
-    }
+function reset () {
+  let size = window.prompt ("Please enter pixel size, 1-64");
+  if (size >= 65 || size == null || size == undefined || size == false || isNaN(size)) {
+    alert ("Please enter a number between 1 and 64");
+  } else if (size <65) {
+    eradicate ();
+    makeRows (size, size);
+  }
+}
+
+function eradicate () {
+  const myNode = document.getElementById("container");
+  while (myNode.firstChild) {
+    myNode.removeChild(myNode.lastChild);
   }
 }
